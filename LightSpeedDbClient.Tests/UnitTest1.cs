c88d008a7b5002c115149f8d43a5b57a98ee4950
viskvortsov <vlad@onthelayer.com>
2025-05-19 13:59:52 +0400

    init

---
+++ LightSpeedDbClient.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using ExampleModels.Currencies;
+using LightSpeedDbClient.Database;
+using LightSpeedDBClient.Postgresql.Database;
+
+namespace LightSpeedDbClient.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        IDatabase db = new PostgresqlDatabase("localhost",5432,"backend", "backend", "mysecretpassword");
+        IConnection connection = await db.OpenConnectionAsync();
+        ITransaction transaction = await connection.BeginTransactionAsync();
+        
+        IManager<Currency> manager = new PostgresqlManager<Currency>(connection, transaction);
+        IEnumerable<Currency> currencies = await manager.GetListAsync(1, 100);
+        
+        Currency currency = manager.Create();
+        
+        
+        await transaction.CommitAsync();
+
+        await transaction.DisposeAsync();
+        await connection.DisposeAsync();
+        await db.DisposeAsync();
+
+    }
+}
